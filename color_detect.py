0293fe28d89e6a5d749b92946cd73f4b0f980c37
Max-Verstappen24 <jayentasingh80@gmail.com>
2024-10-26 15:49:14 +0530

    Update color_detect.py

--- color_detect.py
+++ color_detect.py
@@ -3,19 +3,29 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-mincol = np.array([200,200,0], ndmin=3)
-maxcol = np.array([255,255,0], ndmin=3)
+# [0-180, 0-255, 0-255]
+mincol = np.array([0,100,00], ndmin=3)
+maxcol = np.array([180,255,150], ndmin=3)
 
 while cap.isOpened:
     ret, frame = cap.read()
     if not ret:
         break
 
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    mask = cv2.inRange(rgb_frame, mincol, maxcol)
-    oframe = cv2.bitwise_and(frame, frame, mask = mask)
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_frame, mincol, maxcol)
+    # oframe = cv2.bitwise_and(frame, frame, mask = mask)
 
-    cv2.imshow('color', oframe)
+    # Find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw rectangles around detected contours
+    for contour in contours:
+        if cv2.contourArea(contour) > 100:  # Filter out small contours
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    fframe=cv2.flip(frame, 1)
+    cv2.imshow('color', fframe)
     if cv2.waitKey(5) & 0xFF == 27:  # Exit on 'Esc' key
         break
 
